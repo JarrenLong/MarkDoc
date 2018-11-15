baea65f9226b958c7a1b6c2dfa804a6f2fd865c8
Jarren Long <jarrenlong@gmail.com>
2018-11-14 17:43:48 -0800

    Moving MarkDoc implementation into its own file

--- index.php
+++ index.php
@@ -1,114 +1,3 @@
-<?php
-
-require_once("Parsedown.php");
-
-class MarkDoc {
-
-  protected function startsWith($haystack, $needle) {
-    $length = strlen($needle);
-    return (substr($haystack, 0, $length) === $needle);
-  }
-
-  protected function endsWith($haystack, $needle) {
-    $length = strlen($needle);
-    if ($length == 0) {
-      return true;
-    }
-
-    return (substr($haystack, -$length) === $needle);
-  }
-
-  protected function getDirContents($dir, $fileType, &$results = array()){
-    $files = scandir($dir);
-
-    foreach($files as $key => $value){
-      $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
-      if(!is_dir($path) && $this->endsWith($path, $fileType)) {
-        $results[] = $path;
-      } else if($value != "." && $value != "..") {
-        $this->getDirContents($path, $fileType, $results);
-        if($this->endsWith($path, $fileType)) {
-          $results[] = $path;
-        }
-      }
-    }
-
-    return $results;
-  }
-
-  protected function generateTOC() {
-    // Regen the ToC file
-    $baseDir = "/home/dev/public_html/";
-    $allFiles = $this->getDirContents($baseDir, '.md');
-
-    $toc = fopen("toc.md", "w");
-    fwrite($toc, "# Master Sitemap\r\n");
-    fwrite($toc, "## &copy; 2018 [Books N' Bytes, Inc.](https://www.booksnbytes.net)\r\n\r\n");
-
-    foreach($allFiles as &$p) {
-      $tmp = str_replace($baseDir, '', $p);
-      fwrite($toc, ' * [' . str_replace(".md",'',$tmp) . '](http://' . $_SERVER["SERVER_NAME"] . '?p=' . $tmp . ")\r\n");
-    }
-    fclose($toc);
-  }
-
-  protected function dumpWP() {
-    $q = "SELECT CONCAT('Posted by ', U.display_name, ' at ', P.post_date) as posted_by, P.post_title, P.post_content FROM wp_posts P LEFT JOIN wp_users U ON P.post_author=U.ID WHERE post_type='post' and post_status='publish'";
-
-    $conn = new mysqli('localhost', 'docs', 'P@ssword1!', 'docs');
-    $res = $conn->query($q);
-
-    if($res->num_rows > 0) {
-      while($row = $res->fetch_assoc()) {
-        $fn = "posts/" . str_replace(" ", "_", $row["post_title"]) . ".md";
-        $contents = "# " .
-            $row["post_title"] .
-            "\r\n\r\n### " .
-            $row["posted_by"] .
-            "\r\n\r\n" .
-            $row["post_content"];
-        file_put_contents($fn, $contents);
-      }
-    }
-  }
-
-  public function processRequest() {
-    // Find the requested file
-    $page = htmlspecialchars($_GET["p"]);
-    // Failover to index
-    if($page == null || $page == '') {
-      $page = "index.md";
-    }
-    // No parent relative paths
-    $page = str_replace("..", "", $page);
-    // Make sure we're only looking at markdown files
-    if(!$this->endsWith($page, '.md')) {
-      $page = $page . ".md";
-    }
-    if($page == "dump.md") {
-      // Pull posts out of a WP db and dump them into MD files
-      $this->dumpWP();
-      $page = "tic.md";
-    }
-    if($page == "tic.md") {
-      // Regenerate the TOC
-      $this->generateTOC();
-      $page = "toc.md";
-    }
-    // Failover to index if required file does not exist
-    if(!file_exists($page)) {
-      $page = "index.md";
-    }
-
-    // Read and render the markdown file
-    $md = file_get_contents($page);
-    $Parsedown = new Parsedown();
-    echo $Parsedown->text($md);
-  }
-
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head lang="en">
@@ -173,6 +62,8 @@ class MarkDoc {
       <div class="starter-template" style="padding-top:80px;">
 
 <?php
+require_once("MarkDoc.php");
+
 $md = new MarkDoc();
 $md->processRequest();
 ?>
