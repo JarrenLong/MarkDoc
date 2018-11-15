a5087a89d6c0f1a12b3ab63de787a78e8920521e
Jarren Long <jarrenlong@gmail.com>
2018-11-14 22:06:56 -0800

    Echoing out html

--- index.php
+++ index.php
@@ -1,61 +1,68 @@
+<?php
+
+echo <<<EOF
 <!DOCTYPE html>
 <html>
-<head lang="en">
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="MarkDoc | Books N' Bytes, Inc.">
-  <meta name="author" content="Books N' Bytes, Inc.">
-  <link rel="icon" href="favicon.ico">
+<head lang='en'>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+  <meta name='description' content='MarkDoc | Books N' Bytes, Inc.'>
+  <meta name='author' content='Books N' Bytes, Inc.'>
+  <link rel='icon' href='favicon.ico'>
   <title>MarkDoc | Books N' Bytes, Inc.</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO' crossorigin='anonymous'>
 </head>
 <body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="?p=index">MarkDoc</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav class='navbar navbar-expand-md navbar-dark bg-dark fixed-top'>
+      <a class='navbar-brand' href='?p=index'>MarkDoc</a>
+      <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarsExampleDefault' aria-controls='navbarsExampleDefault' aria-expanded='false' aria-label='Toggle navigation'>
+        <span class='navbar-toggler-icon'></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="?p=index">Home <span class="sr-only">(current)</span></a>
+      <div class='collapse navbar-collapse' id='navbarsExampleDefault'>
+        <ul class='navbar-nav mr-auto'>
+          <li class='nav-item active'>
+            <a class='nav-link' href='?p=index'>Home <span class='sr-only'>(current)</span></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="?p=toc">Table of Contents</a>
+          <li class='nav-item'>
+            <a class='nav-link' href='?p=toc'>Table of Contents</a>
           </li>
-          <!-- <li class="nav-item"><a class="nav-link disabled" href="#">Disabled</a></li> -->
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Help</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="?p=README">About MarkDoc</a>
-			  <a class="dropdown-item" href="?p=changelog">Changelog</a>
+          <!-- <li class='nav-item'><a class='nav-link disabled' href='#'>Disabled</a></li> -->
+          <li class='nav-item dropdown'>
+            <a class='nav-link dropdown-toggle' id='dropdown01' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>Help</a>
+            <div class='dropdown-menu' aria-labelledby='dropdown01'>
+              <a class='dropdown-item' href='?p=README'>About MarkDoc</a>
+			  <a class='dropdown-item' href='?p=changelog'>Changelog</a>
             </div>
           </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <form class='form-inline my-2 my-lg-0'>
+          <input class='form-control mr-sm-2' type='text' placeholder='Search' aria-label='Search'>
+          <button class='btn btn-outline-success my-2 my-sm-0' type='submit'>Search</button>
         </form>
       </div>
     </nav>
 
-    <main role="main" class="container">
-      <div class="starter-template" style="padding-top:80px;">
+    <main role='main' class='container'>
+      <div class='starter-template' style='padding-top:80px;'>
 
-<?php
-require_once("MarkDoc.php");
+EOF;
+
+require_once('MarkDoc.php');
 
 $md = new MarkDoc();
 $md->processRequest();
-?>
+
+echo <<<EOF
 
       </div>
     </main>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
+    <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' integrity='sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49' crossorigin='anonymous'></script>
+    <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js' integrity='sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy' crossorigin='anonymous'></script>
 </body>
 </html>
+
+EOF;
