aaa22d73a069fb077f922e97ac67b71d3aa70e8a
Jarren Long <jarrenlong@gmail.com>
2018-11-19 12:56:40 -0800

    Adding icon

--- index.php
+++ index.php
@@ -5,13 +5,15 @@
   <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
   <meta name='description' content='MarkDoc | Books N' Bytes, Inc.'>
   <meta name='author' content='Books N' Bytes, Inc.'>
-  <link rel='icon' href='favicon.ico'>
+  <link rel='icon' href='markdoc.png'>
   <title>MarkDoc | Books N' Bytes, Inc.</title>
   <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO' crossorigin='anonymous'>
 </head>
 <body>
     <nav class='navbar navbar-expand-md navbar-dark bg-dark fixed-top'>
-      <a class='navbar-brand' href='https://www.booksnbytes.net'>MarkDoc</a>
+      <a class='navbar-brand' href='https://www.booksnbytes.net'>
+        <img src='markdoc.png' alt='MarkDoc' height='44px' />
+      </a>
       <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarsExampleDefault' aria-controls='navbarsExampleDefault' aria-expanded='false' aria-label='Toggle navigation'>
         <span class='navbar-toggler-icon'></span>
       </button>
